7d9978e5dd57dec8f5254f027494bee73117646d
jusufnovalic <117160230+jusufnovalic@users.noreply.github.com>
2023-08-18 18:35:09 +0200

    Add files via upload

---
+++ r/basics.r
@@ -0,0 +1,25 @@
+
+x <- iris #varijabla x sadrzi ceo iris
+
+setosa <- iris[iris$Species == "setosa",] #setosa sadrzi svaki red vrste setosa
+versicolor <- iris[iris$Species == "versicolor",] #versicolor sadrzi svaki red vrste versicolor
+
+setosa
+versicolor
+nrow(versicolor) #broj redova te vrste 50
+nrow(setosa)
+nrow(iris) # br redova celog irisa 150
+ncol(iris) # broj colona 5
+
+
+t.test(x =setosa$Petal.Length , y = versicolor$Petal.Length) # uporedjivanje dve kolone dve razlicite vrste
+
+#  Challenge 1 
+
+# Test for significant differences in petal lengths between
+#  I. setosa and I. virginica and between I. versicolor and I. virginica.
+
+#  code: 
+
+
+
